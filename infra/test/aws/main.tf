f3f4ed20e57e7b895e3723df598b9dffaee44357
ryougi-shiky <ryougikalan@gmail.com>
2025-09-28 20:01:36 +0800

    fix: update terraform to 6.0

--- infra/test/aws/main.tf
+++ infra/test/aws/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.3"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = { source = "hashicorp/aws", version = ">= 6.0" }
   }
 }
 
