a0a2e5ff0206048f0c1d047f86caa45ea5c11e49
ryougi-shiky <ryougikalan@gmail.com>
2025-09-30 10:58:08 +0800

    fix: temporarily add post destroy for test

--- infra/test/aws/main.tf
+++ infra/test/aws/main.tf
@@ -27,7 +27,7 @@ module "github_actions_oidc_role" {
   aws_account_id = "219396432881"
   github_owner   = "ryougi-shiky"
   github_repo    = "COMP30022-IT-Project"
-  github_branch  = "deploy-app-staging"
+  github_branch  = "deploy-test-app"
 }
 
 
