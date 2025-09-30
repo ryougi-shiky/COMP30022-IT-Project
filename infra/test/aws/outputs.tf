55b2a256edd7180e1766f3b0c2bc688184b10502
ryougi-shiky <ryougikalan@gmail.com>
2025-09-30 10:30:18 +0800

    fix: rm app outputs

--- infra/test/aws/outputs.tf
+++ infra/test/aws/outputs.tf
@@ -14,18 +14,6 @@ output "ecs_task_execution_role_arn" {
   value = aws_iam_role.ecs_task_execution.arn
 }
 
-output "mongo_efs_id" {
-  value = aws_efs_file_system.mongo.id
-}
-
-output "mongo_ap_id" {
-  value = aws_efs_access_point.mongo_ap.id
-}
-
-output "alb_sg_id" {
-  value = aws_security_group.alb.id
-}
-
 output "ecs_sg_id" {
   value = aws_security_group.ecs.id
 }
