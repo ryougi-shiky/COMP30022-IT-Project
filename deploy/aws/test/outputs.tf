7524b56603bc159cfe42659e3fc7e5bbfbb6c40e
ryougi-shiky <ryougikalan@gmail.com>
2025-10-09 15:32:20 +0800

    feat: tf implemented by gpt..

---
+++ deploy/aws/test/outputs.tf
@@ -0,0 +1,11 @@
+output "task_definition_arn" {
+  value = aws_ecs_task_definition.app.arn
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
+
+output "ecs_service_arn" {
+  value = aws_ecs_service.app.arn
+}
