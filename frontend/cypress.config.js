34d0d96f63efe7f4f51b6d733be09f17e22d98b3
ryougi-shiky <ryougikalan@gmail.com>
2025-10-14 14:40:19 +0800

    chore: update cypress config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,15 +1,20 @@
 const { defineConfig } = require("cypress");
 
-const appBaseUrl = process.env.APP_BASE_URL || 'http://localhost:3000';
-
 module.exports = defineConfig({
+  // Default timeout for most commands, can be overridden by env var
+  defaultCommandTimeout: process.env.CYPRESS_DEFAULT_COMMAND_TIMEOUT
+    ? parseInt(process.env.CYPRESS_DEFAULT_COMMAND_TIMEOUT)
+    : 8000,
+
   e2e: {
-    baseUrl: appBaseUrl,
+    // This is the default baseUrl.
+    // It can be overridden by the CYPRESS_BASE_URL environment variable.
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   env: {
-    apiUrl: 'http://localhost:17000',
-  }
+    // You can add other environment variables here
+  },
 });
