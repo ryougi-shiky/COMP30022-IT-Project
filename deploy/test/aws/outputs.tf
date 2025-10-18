fb626e1540d0daa3b8b86c08b4647227eab76f60
ryougi-shiky <ryougikalan@gmail.com>
2025-10-18 10:05:08 +0800

    feat: refactor ECS task public IP retrieval and remove unused resources

--- deploy/test/aws/outputs.tf
+++ deploy/test/aws/outputs.tf
@@ -14,8 +14,3 @@ output "cluster_name" {
   description = "ECS cluster name for querying task details"
   value       = var.cluster_id != "" ? var.cluster_id : split("/", var.cluster_arn)[1]
 }
-
-output "task_public_ip" {
-  description = "Public IP address of the ECS task"
-  value       = trimspace(data.local_file.task_public_ip.content)
-}
